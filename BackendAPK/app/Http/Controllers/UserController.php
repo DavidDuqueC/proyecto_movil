c26cb989264d03f3fc592f0ddb850fe6928d5f64
David Duque <davduqueca@unal.edu.co>
2026-04-02 11:40:01 -0500

    feat(auth): implement Laravel Sanctum authentication for register, login and logout
    - Configure Laravel Sanctum
    - Create UerController with register, login and logout methods
    - Add API routes for authentication endpoints
    - Implement token-based authentication using Sanctum
    - Add validation rules for registration and login requests
    - Protect routes that require authenticated users

--- BackendAPK/app/Http/Controllers/UserController.php
+++ BackendAPK/app/Http/Controllers/UserController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+    $user = User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>Hash::make($request->password),
+    ]);return response()->json([
+        'ok'=> true,
+        'message'=>'Usuario registrado'
+    ], 201);
+    }
+    public function login(Request $request){
+    $user= User::where('email', $request->email)->first();
+    if (! $user || !Hash::check(
+            $request->password,
+            $user->password,
+        ) ){
+    return response()->json([
+        'message'=>'Credenciales invalidas'
+        ], 401);
+        }
+    $token = $user->createToken('auth_token')->plainTextToken;
+    return response()->json([
+        'token'=>$token,
+        'type'=>'Bearer'
+    ]);
+
+    }
+    public function logout(Request $request){
+        $request->user()->
+        currentAccessToken()->
+        delete();
+        return response()->json(['ok' => true]);
+    }
 }
--- BackendAPK/routes/api.php
+++ BackendAPK/routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\UserController;
 
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 Route::get('movies' , [MovieController::class, 'index']);
 Route::post('movies', [MovieController::class, 'store']);
 Route::delete('movies/{id}', [MovieController::class, 'destroy']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware("auth:sanctum");
